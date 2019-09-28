c91e8f7bdf691841187595dee17ae2e699acee34
Mario S <marionbxcz@gmail.com>
2019-09-27 22:10:17 -0300

    Update Cadastrar

--- projeto_final/lib/cadastraEmails.dart
+++ projeto_final/lib/cadastraEmails.dart
@@ -4,19 +4,33 @@ class CadastraEmail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar(
-        backgroundColor: Colors.cyan,
-        leading: Builder(
-            builder: (BuildContext context){
-              return IconButton(
-                icon: Icon(Icons.arrow_back),
-                onPressed: () {
+    return MaterialApp(
+      home: Scaffold (
+        appBar: AppBar(
+          backgroundColor: Colors.cyan,
+          leading: Builder(
+              builder: (BuildContext context){
+                return IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
 
-                },
-              );
-            }),
-        title: Text("Cadastrar e-mail"),
+                  },
+                );
+              }),
+          title: Text("Cadastrar e-mail"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Center (
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("VOCE"),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
